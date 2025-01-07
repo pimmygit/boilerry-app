50625a50747afee53701697799ac0fb475d324d2
Kliment Stefanov <kliment@hotmail.co.uk>
2025-01-07 22:10:23 +0000

    Move the graphing code out of the ActivityMain, into its own class.
    Also update the imported libraries and the target SDK.

--- app/src/main/java/com/vayak/boilerry/ActivityMain.kt
+++ app/src/main/java/com/vayak/boilerry/ActivityMain.kt
@@ -8,15 +8,7 @@ import android.widget.*
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.CombinedChart
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis.AxisDependency
-import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.vayak.boilerry.Constants.Companion.MASTER_SWITCH_STATE_OFF
 import com.vayak.boilerry.Constants.Companion.MASTER_SWITCH_STATE_ON
 import com.vayak.boilerry.Constants.Companion.MASTER_SWITCH_STATE_PREDICTIVE
@@ -27,8 +19,6 @@ import com.vayak.boilerry.databinding.ActivityMainBinding
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocket
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.collections.ArrayList
 
 
@@ -38,7 +28,7 @@ class ActivityMain : AppCompatActivity() {
     //private var cHOST:                          String?             = "192.168.1.7"
     //private var cHOST:                          String?             = "92.13.183.194"
     private var cPORT:                          Int                 = 9741
-    private val cPROTOCOL:                      String?             = "ws"
+    private val cPROTOCOL:                      String              = "ws"
     private val cURL:                           String              = "$cPROTOCOL://$cHOST:$cPORT"
 
     private var blockOnChangeListener:          Boolean?            = false
@@ -51,19 +41,17 @@ class ActivityMain : AppCompatActivity() {
     private lateinit var binding:               ActivityMainBinding
     private lateinit var viewModel:             ThermostatViewModel
 
-    private lateinit var mToast:                Toast;
+    private lateinit var mToast:                Toast
 
     @SuppressLint("UseSwitchCompatOrMaterialCode", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("onCreate", "Starting")
         super.onCreate(savedInstanceState)
 
-        // TODO: Do it properly
-        var graphData = ArrayList<TempHistory>()
         var graphBuilt = false
 
         // Initialise here to speed up the pop-up refresh
-        mToast = Toast.makeText(applicationContext, "", Toast.LENGTH_SHORT);
+        mToast = Toast.makeText(applicationContext, "", Toast.LENGTH_SHORT)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -95,7 +83,10 @@ class ActivityMain : AppCompatActivity() {
             if (!graphBuilt) {
                 // The graph data wont change really - there is need to re-build it every time.
                 graphBuilt = true
-                drawGraph(it.temp_history as ArrayList<TempHistory>)
+
+                val combinedChart = findViewById<View>(R.id.temperature_chart) as CombinedChart
+                val tempHistory = it.temp_history as ArrayList<TempHistory>
+                GraphSummary.drawGraphSummary(combinedChart, tempHistory)
             }
 
             blockOnChangeListener = false
@@ -169,112 +160,6 @@ class ActivityMain : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    fun String.toTimeDateLong(): Long {
-        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.UK)
-        return format.parse(this)?.time ?: throw IllegalArgumentException("Invalid time string")
-    }
-
-    private fun drawGraph(tempHistoryData: ArrayList<TempHistory>) {
-        //Part1
-        val entriesIn = ArrayList<Entry>()
-        val entriesOut = ArrayList<Entry>()
-        val entriesOn = ArrayList<BarEntry>()
-
-        Log.d("onCreate","Number of graph records: ${tempHistoryData.size}")
-
-        var index = 1f
-        val labelList = ArrayList<String>()
-        for (tempMeasurement in tempHistoryData) {
-            if (tempMeasurement.sensor_1 != null && tempMeasurement.temperature != null) {
-                entriesIn.add(Entry(index, tempMeasurement.sensor_1))
-                entriesOut.add(Entry(index, tempMeasurement.temperature))
-                entriesOn.add(BarEntry(index, tempMeasurement.time_state_on.toFloat()))
-                labelList.add(tempMeasurement.datetime)
-                index++
-            }
-        }
-        //Part2
-
-        //Part3
-        val ldsIn = LineDataSet(entriesIn, "Inside")
-        ldsIn.axisDependency = AxisDependency.LEFT
-        val ldsOut = LineDataSet(entriesOut, "Outside")
-        ldsOut.axisDependency = AxisDependency.LEFT
-        val ldsOn = BarDataSet(entriesOn, "Heating")
-        ldsOn.axisDependency = AxisDependency.RIGHT
-
-        //Part4
-        ldsIn.setDrawValues(false)
-        ldsIn.setDrawFilled(true)
-        ldsIn.setDrawCircles(false)
-        ldsIn.lineWidth = 3f
-        //ldsIn.color = R.color.blue_ivy
-        ldsIn.fillColor = R.color.alice_blue
-        ldsIn.fillAlpha = 50
-
-        ldsOut.setDrawValues(false)
-        ldsOut.setDrawFilled(true)
-        ldsOut.setDrawCircles(false)
-        ldsOut.lineWidth = 3f
-        ldsOut.color = R.color.blue
-        ldsOut.fillColor = R.color.aqua
-        ldsOut.fillAlpha = 100
-
-        ldsOn.color = R.color.orange
-        ldsOn.barBorderColor = R.color.orange_red
-
-        //Part5
-        val combinedChart = findViewById<View>(R.id.temperature_chart) as CombinedChart
-
-        combinedChart.xAxis.labelRotationAngle = 0f
-
-        //Part6
-        val dataSets = ArrayList<ILineDataSet>()
-        dataSets.add(ldsIn)
-        dataSets.add(ldsOut)
-
-        val combinedData = CombinedData()
-        combinedData.setData(LineData(dataSets))
-        combinedData.setData(BarData(ldsOn))
-
-        combinedChart.data = combinedData
-        combinedChart.invalidate()
-
-        // Add the datetime as labels
-        val formatter: ValueFormatter = object : ValueFormatter() {
-            override fun getAxisLabel(value: Float, axis: AxisBase): String {
-                return try {
-                    labelList[value.toInt()].drop(8).dropLast(3)
-                } catch (oob: IndexOutOfBoundsException) {
-                    labelList[0].drop(8).dropLast(3)
-                }
-            }
-        }
-        val xAxis: XAxis = combinedChart.xAxis
-        xAxis.granularity = 1f // minimum axis-step (interval) is 1
-        xAxis.valueFormatter = formatter
-
-        //Part7
-        combinedChart.axisRight.isEnabled = false
-        //lineChart.xAxis.axisMaximum = j+0.1f
-
-        //Part8
-        //lineChart.setTouchEnabled(true)
-        //lineChart.setPinchZoom(true)
-
-        //Part9
-        combinedChart.description.text = "Days"
-        combinedChart.setNoDataText("No forex yet!")
-
-        //Part10
-        combinedChart.animateX(1800, Easing.EaseInExpo)
-
-        //Part11
-        //val markerView = CustomMarker(this@ShowForexActivity, R.layout.marker_view)
-        //lineChart.marker = markerView
-
     }
 
     //Called when the activity is about to become visible
