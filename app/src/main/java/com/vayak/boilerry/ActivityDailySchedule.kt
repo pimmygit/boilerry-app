08c1c77960940cce0b103f83040019ef23157cfb
Kliment Stefanov <kliment@hotmail.co.uk>
2025-01-27 23:18:19 +0000

    Add Daily Schedule screen (Mock/Playground).

--- app/src/main/java/com/vayak/boilerry/ActivityDailySchedule.kt
+++ app/src/main/java/com/vayak/boilerry/ActivityDailySchedule.kt
@@ -1,7 +1,11 @@
 package com.vayak.boilerry
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
 
 
 class ActivityDailySchedule : AppCompatActivity() {
@@ -9,7 +13,32 @@ class ActivityDailySchedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_schedule)
-        // finding the UI elements
 
+        val pickerFrom = findViewById<View>(R.id.time_picker_from) as TimePicker
+        pickerFrom.setIs24HourView(true)
+        val pickerTo = findViewById<View>(R.id.time_picker_to) as TimePicker
+        pickerTo.setIs24HourView(true)
+
+        val seekbarFieldTimeSlotTemperature = findViewById<SeekBar>(R.id.seekTimeSlotTemperature)
+        // Start listening for user action on the thermostat setting
+        seekbarFieldTimeSlotTemperature.setOnSeekBarChangeListener(object :
+            OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+                //if (blockOnChangeListener != true) {
+                //    Log.d("onCreate", "Sending command: setThermostat(${seekBar.progress})")
+                //    webSocket?.send(setThermostatTemperature(seekBar.progress).toString())
+                //}
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Popup the current value as a message
+                //mToast.setText(progress.toString())
+                //mToast.show()
+            }
+        })
     }
 }
