d8748aff1971203e3d27054d4a38346483b26455
Kliment Stefanov <kliment@hotmail.co.uk>
2025-06-04 23:57:41 +0100

    Move the graphing code out of the ActivityMain, into its own class.
    Also update the imported libraries and the target SDK.
    
    Programmatically create the temperature time slot table from a hardcoded List, with the single functionality to delete a selected row

--- app/src/main/java/com/vayak/boilerry/ActivityDailySchedule.kt
+++ app/src/main/java/com/vayak/boilerry/ActivityDailySchedule.kt
@@ -1,9 +1,17 @@
 package com.vayak.boilerry
 
+import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +24,72 @@ class ActivityDailySchedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_schedule)
+
+        val dailyTemperatureScheduleTable: TableLayout? = findViewById(R.id.dailyTemperatureSchedule)
+
+        val dailyTemperatures: List<Triple<Double, String, String>> = listOf(
+            Triple(17.0, "00:00", "06:00"),
+            Triple(21.0, "06:00", "08:30"),
+            Triple(19.0, "08:30", "16:30"),
+            Triple(21.0, "16:30", "22:00"),
+            Triple(17.0, "22:00", "24:00")
+        )
+
+        // Print each tuple
+        for (dailyTemperature in dailyTemperatures) {
+            Log.d("onCreate", "Adding schedule [${dailyTemperature.second}]-[${dailyTemperature.third}]")
+
+            val tableRowTemperatureTimeSlot = TableRow(this).apply {
+                setBackgroundColor(Color.parseColor("#F0F7F7"))
+                setPadding(5, 5, 5, 5)
+            }
+
+            val tableColumnFrom = TextView(this).apply {
+                text = dailyTemperature.second
+                layoutParams = TableRow.LayoutParams(50, TableRow.LayoutParams.WRAP_CONTENT, 1F);
+            }
+
+            val tableColumnTo = TextView(this).apply {
+                text = dailyTemperature.third
+                layoutParams = TableRow.LayoutParams(50, TableRow.LayoutParams.WRAP_CONTENT, 1F);
+            }
+
+            val tableColumnTemperature = TextView(this).apply {
+                text = "${dailyTemperature.first}* C"
+                layoutParams = TableRow.LayoutParams(30, TableRow.LayoutParams.WRAP_CONTENT, 1F);
+            }
+
+            val tableColumnButton = Button(this).apply {
+                text = "Del"
+                layoutParams = TableRow.LayoutParams(30, TableRow.LayoutParams.WRAP_CONTENT, 1F);
+            }
+            tableColumnButton.setOnClickListener {
+                // Confirmation dialog before deleting the entry.
+                val builder = AlertDialog.Builder(this@ActivityDailySchedule)
+                builder.setMessage("Are you sure you want to Delete?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes") { dialog, id ->
+                        // Confirmed -> Remove the current row from the table
+                        // todo: Prior that, we have to also remove the entry from the DB
+                        val container = (tableRowTemperatureTimeSlot.parent as ViewGroup)
+                        container.removeView(tableRowTemperatureTimeSlot)
+                        container.invalidate()
+                    }
+                    .setNegativeButton("No") { dialog, id ->
+                        // Dismiss the dialog
+                        dialog.dismiss()
+                    }
+                val alert = builder.create()
+                alert.show()
+            }
+
+            tableRowTemperatureTimeSlot.addView(tableColumnFrom)
+            tableRowTemperatureTimeSlot.addView(tableColumnTo)
+            tableRowTemperatureTimeSlot.addView(tableColumnTemperature)
+            tableRowTemperatureTimeSlot.addView(tableColumnButton)
+
+            dailyTemperatureScheduleTable?.addView(tableRowTemperatureTimeSlot)
+        }
 
         // Initialise here to speed up the pop-up refresh
         mToast = Toast.makeText(applicationContext, "", Toast.LENGTH_SHORT)
